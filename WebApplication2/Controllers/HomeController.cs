63d48c3b321b75914fefbaee11e20f6acaa35389
Carlos Aracena <carlosthefather2014@hotmail.com>
2019-03-21 22:38:54 -0400

    Primer Commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DatosPersonales()
+        {
+            Form modelo = new Form();
+            modelo.Name = Request.Form["Name"].ToString();
+            modelo.Cedula = Request.Form["Cedula"].ToString();
+            modelo.Age = int.Parse(Request.Form["age"]);
+            modelo.Tel = Request.Form["Tel"].ToString();
+            modelo.Email = Request.Form["Email"].ToString();
+            modelo.Gender = Request.Form["Gender"].ToString();
+            modelo.Last = Request.Form["Last"].ToString();
+            modelo.CivilStatus = "Casado";
+            String Hobbys = "";
+            if (modelo.Hobbys == true)
+            {
+                Hobbys = "No Tengo Hobbys";
+            }
+
+            Hobbys = "Si Tengo Hobbys";
+            modelo.HobbieDefinitivo = Hobbys;
+
+            return View(modelo);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Form.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Form
+    {
+        [Required(ErrorMessage ="El campo es obligatorio")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public String Cedula { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [Range(15, 100, ErrorMessage = "La edad debe ser mayor a 15 y menor a 100")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public String Tel { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public String Gender { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public String Last { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public String CivilStatus { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public bool Hobbys { get; set; }
+        public String HobbieDefinitivo { get; set; }
+
+    }
+}
